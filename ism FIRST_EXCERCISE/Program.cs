1241c7d7ffc6f4d8df9582bcf7bb07fb5ef39117
Adeyemi Omisakin <aomisakin@usf.edu>
2020-01-24 12:01:04 -0500

    Add project files.

---
+++ ism FIRST_EXCERCISE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ism_FIRST_EXCERCISE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
